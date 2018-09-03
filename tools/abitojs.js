09489ad68d28b6d0ac5200cc9aac9e244e88cbd3
romanmandryk <roman.mandryk@gmail.com>
2018-09-03 14:53:56 +1000

    improved generator, added documentation scraper

--- tools/abitojs.js
+++ tools/abitojs.js
@@ -1,12 +1,22 @@
 const contracts = require('../lib/abis/index');
 const fs = require('fs');
+const docsDescriptions = require('../lib/docSrc/descriptions');
 const typeMap = {
-  uint256: 'Number',
+  uint256: 'BigNumber',
   uint8: 'Number',
   address: 'String<EthAddress>',
   bool: 'boolean',
   string: 'String',
 };
+
+const contractAbiMap = {
+  Nomin: 'Nomin',
+  Havven: 'Havven',
+  IssuanceController: 'IssuanceController',
+  StablePayments: 'Nomin',
+  Mintr: 'Havven',
+}
+
 
 //console.log(Havven);
 const generate = () => {
@@ -17,8 +27,7 @@ const generate = () => {
 };
 
 const generateJSFile = (contractName, functions) => {
-  const content = `const {Contract, providers, utils} = require('ethers');
-const addresses = require('../../lib/addresses');
+  const content = `const {Contract} = require('ethers');
 const abi = require('../../lib/abis').${contractName};
 const ContractSettings = require('../contractSettings');
 
@@ -34,12 +43,12 @@ function ${contractName}(contractSettings) {
     this.contractSettings.signer || this.contractSettings.provider
   );
   
-  ${functions.map(fn => generateFunctionStr(fn)).join('')}
+  ${functions.map(fn => generateFunctionStr(fn, contractName)).join('')}
 
 }
 
 module.exports = ${contractName}`;
-  fs.writeFile(`${__dirname}/../contracts/${contractName}.js`, content, err => {
+  fs.writeFile(`${__dirname}/../src/contracts/${contractName}.js`, content, err => {
     if (err) {
       console.log(err);
     } else {
@@ -50,7 +59,9 @@ module.exports = ${contractName}`;
 };
 
 getFnParams = params => {
-  return params.map(p => p.name).join(', ');
+  return params.map(p => {
+    return (p.name) ? p.name: p.type;
+  }).join(', ');
 };
 
 getJsdocParam = param => {
@@ -63,35 +74,50 @@ getJsdocReturns = outputs => {
     return ` * @returns ${typeMap[outputs[0].type] || outputs[0].type}`;
   }
   return ` * @returns Object`;
-  /*return ` * @returns [${outputs.map(output => {
-    return `{${output.name}: ${typeMap[output.type] || output.type}}`
-  }).join(', ')}]`*/
 };
 
-generateJsdoc = abiFn => {
-  const inputParams = abiFn.inputs.length
-    ? '\n' + abiFn.inputs.map(input => getJsdocParam(input)).join('\n')
-    : '';
+generateJsdoc = (abiFn, params, contractName) => {
+  let description = docsDescriptions[contractAbiMap[contractName]][abiFn.name];
+  description = (description) ? description + '<br>\n   * ': '';
   const constantStr = abiFn.constant
-    ? "constant - doesn't require signer"
-    : 'function - requires signer';
+    ? "Call (no gas consumed, doesn't require signer)"
+    : 'Transaction (consumes gas, requires signer)';
+  const payable = abiFn.payable
+    ? "\n<br>Payable (to enter ETH amount set txParams.value)"
+    : '';
+  const inputParams = params.length
+    ? '\n' + params.map(input => getJsdocParam(input)).join('\n')
+    : '';
   return `
   /**
-   * ${abiFn.name} - ${constantStr}${inputParams}
+   * ${description}${constantStr}${payable}${inputParams}
   ${getJsdocReturns(abiFn.outputs)}
    **/`;
 };
 
-const generateFunctionStr = abiFn => {
-  const params = getFnParams(abiFn.inputs);
-  const jsdoc = generateJsdoc(abiFn);
-
-  return `
+const generateFunctionStr = (abiFn, contractName) => {
+  let params = [...abiFn.inputs];
+  if (!abiFn.constant){
+    params.push({name: 'txParams', type: 'TxParams'});
+  }
+  const paramsStr = getFnParams(params);
+  const jsdoc = generateJsdoc(abiFn, params, contractName);
+  if (!abiFn.constant){
+    return `
 ${jsdoc}
-  this.${abiFn.name} = async (${params}) => {
-    return await this.contract.${abiFn.name}(${params});
+  this.${abiFn.name} = async (${paramsStr}) => {
+    txParams = txParams || {};
+    return await this.contract.${abiFn.name}(${paramsStr});
   };
 `;
+  } else {
+    return `
+${jsdoc}
+  this.${abiFn.name} = async (${paramsStr}) => {
+    return await this.contract.${abiFn.name}(${paramsStr});
+  };
+`;
+  }
 };
 
 generate();
---
+++ tools/scrapeDocs.js
@@ -0,0 +1,32 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+const baseUrl = 'http://developer.havven.io/api/docs/';
+const contractNames  = ['Havven', 'Nomin', 'IssuanceController'];
+const docsObj = {};
+const writeFile = require('util').promisify(fs.writeFile);
+
+async function getDocsForContract(contractName) {
+  const res = await axios.get(baseUrl + contractName);
+  const $ = cheerio.load(res.data);
+  docsObj[contractName] = {};
+  $('.item').each((index, item) => {
+    const name = $(item).find('h4').text();
+    const description = $(item).find('.body > .description > p').text();
+    docsObj[contractName][name] = description;
+  });
+}
+
+async function start(){
+  try{
+    const done = await Promise.all(contractNames.map(name => getDocsForContract(name)));
+    console.log(docsObj)
+    await fs.writeFile(__dirname + '/../lib/docSrc/descriptions.json', JSON.stringify(docsObj, null, 2));
+    console.log('done');
+  } catch (e){
+    console.log(e)
+  }
+
+}
+
+start();

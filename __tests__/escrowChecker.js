bbf41314101c9f16101db0ee3150293bdd253924
Clement Balestrat <>
2019-01-14 14:32:10 +1100

    mainNet addresses added for sCHF and sGBP

--- __tests__/escrowChecker.js
+++ __tests__/escrowChecker.js
@@ -3,7 +3,6 @@ import config from './config';
 const havjs = new HavvenJs({ networkId: config.networkId });
 
 test('Should return escrow contract address', async () => {
-  // const escrowAddress = await havjs.EscrowChecker.havven_escrow();
-  // console.log(escrowAddress);
-  // return expect(havjs.utils.formatEther(escrowAddress)).toBeTruthy();
+  const escrowAddress = await havjs.EscrowChecker.havven_escrow();
+  return expect(havjs.utils.formatEther(escrowAddress)).toBeTruthy();
 });

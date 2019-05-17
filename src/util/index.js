bc9ca957d64a2f97c4d8ea88d8f06ec5f1683fd2
Clement Balestrat <>
2019-05-17 16:05:25 +1000

    Ledger + Trezor signers fixed

--- src/util/index.js
+++ src/util/index.js
@@ -67,6 +67,7 @@ class Util {
     transaction.chainId = this.contractSettings.networkId;
 
     const signedTx = await this.contractSettings.signer.sign(transaction);
+    console.log('signed', signedTx, this.contractSettings.provider);
     const signedSerialziedTx = '0x' + signedTx.serialize().toString('hex');
     return await this.contractSettings.provider.sendTransaction(signedSerialziedTx);
   }

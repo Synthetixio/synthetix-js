f50399c3074a42e17e505022cbd08a76d4f02316
justin j. moses <justinjmoses@gmail.com>
2019-07-03 01:35:42 -0400

    Uncommenting issuance ratio tests

--- __tests__/src/contracts/SynthetixState.js
+++ __tests__/src/contracts/SynthetixState.js
@@ -12,8 +12,7 @@ describe(`src/contracts/${contract}`, () => {
       snxjs = new SynthetixJs({ networkId });
     });
 
-    //TODO reenable this test after MAINNET Release
-    xtest(
+    test(
       `${network} Should return target issuance ratio (0.133333333333333333)`,
       async () => {
         const issuanceRatio = await snxjs[contract].issuanceRatio();

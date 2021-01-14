2436b9e922e28001615ce42b598241a3362bbd8b
Synthetix Team <team@synthetix.io>
2021-01-14 21:02:58 +0000

    Prepublish step

--- src/contracts/kovan/index.js
+++ src/contracts/kovan/index.js
@@ -20,6 +20,7 @@ import BinaryOptionMarketData from './BinaryOptionMarketData';
 import SynthUtil from './SynthUtil';
 import EtherCollateralsUSD from './EtherCollateralsUSD';
 import Liquidations from './Liquidations';
+import SynthetixBridgeToOptimism from './SynthetixBridgeToOptimism';
 import SystemSettings from './SystemSettings';
 import sUSD from './sUSD';
 import sEUR from './sEUR';
@@ -85,6 +86,7 @@ export default {
   SynthUtil,
   EtherCollateralsUSD,
   Liquidations,
+  SynthetixBridgeToOptimism,
   SystemSettings,
   sUSD,
   sEUR,
